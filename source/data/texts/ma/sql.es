fb513023a0068d2710eb19d279a963437d7642b5
dmelian <domingo@meliangonzalez.com>
2013-11-06 16:22:22 +0000

    forgotten files

---
+++ source/data/texts/ma/sql.es
@@ -0,0 +1,12 @@
+
+[connection]
+#ERR_SERIALIZEFORM	Serialize form error (form: $_form)
+
+ERR_MYSQLIERROR		ERROR DE INICIALIZACIÓN :: La librería mysqli de acceso a la base de datos, no ha podido ser inicializada
+ERR_UNKNOWNHOST		SERVIDOR NO ENCONTRADO :: El servidor "$_host" no responde o no ha sido configurado
+ERR_ACCESSDENIED	ACCESO DENEGADO :: No se concede el acceso al usuario "$_user"
+ERR_UNKNOWNDATABASE	BASE DE DATOS DESCONOCIDA :: No se encuentra la base de datos "$_database"
+ERR_DBGENERICERROR	ERROR DE BASE DE DATOS :: Error "$_errno": "$_error"
+ERR_DEFAULTSUCCESS	ACCIÓN TERMINADA :: $_message
+ERR_DEFAULTERROR	ERROR EN LA ACCIÓN :: $_message
+
---
+++ source/server/ma/sql/resultIterator.php
@@ -0,0 +1,57 @@
+<?php
+/*
+	This iterator is based on code found in
+ 	http://techblog.procurios.nl/k/news/view/33914/14863/Syntactic-Sugar-for-MySQLi-Results-using-SPL-Iterators.html
+  	By Werner Segers
+*/
+
+
+class ma_sql_resultIterator implements Iterator{
+	protected $result;
+	protected $fetchMode;
+	protected $position;
+	protected $currentRow;
+	protected $closed;
+	
+	//$fetchMode (MYSQLI_ASSOC, MYSQLI_NUM, MYSQLI_BOTH)
+	public function __construct($result, $fetchMode = MYSQLI_ASSOC){
+		$this->result= $result;
+		$this->fetchMode= $fetchMode;
+		$this->closed= false;
+		$this->rewind();
+	}
+	
+	public function close(){		
+		if (!$this->closed) {
+			$this->result->free();
+			$this->closed= true;
+		}
+	}
+	
+	public function __destruct(){ $this->close(); }
+	
+	public function rewind(){
+		$this->result->data_seek($this->position = 0);
+		$this->currentRow = $this->result->fetch_array($this->fetchMode);
+	}
+	
+	public function next(){
+		$this->currentRow = $this->result->fetch_array($this->fetchMode);
+		++$this->position;
+	}
+	
+	public function valid(){
+		return $this->position < $this->result->num_rows;
+	}
+	
+	public function current(){
+		return $this->currentRow;
+	}
+	
+	public function key(){
+		return $this->position;
+	}
+	
+	
+}
+?>
