f756253201148f097db65dc1b101f66f0a43d600
Domingo Melian <domingo@meliangonzalez.com>
2013-11-10 05:55:43 +0000

    cleaning

--- source/server/pos/wdg/button.php
+++
@@ -1,8 +0,0 @@
-<?php
-class pos_wdg_button extends pos_wdg_widget{
-	
-	public function OnPaint( $document ){
-		$document->output('<p>my button</p>');
-	}
-	
-}
--- source/server/pos/wdg/widget.php
+++
@@ -1,4 +0,0 @@
-<?php
-class pos_wdg_widget extends ma_object{
-	
-}
